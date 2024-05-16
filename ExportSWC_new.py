8ca59d5c13b3c17d0fa5427c946955b8a1d8abe4
Biswanath Saha <bsaha0659@gmail.com>
2024-05-16 18:21:33 +0900

    fixed minor bugs

---
+++ ExportSWC_new.py
@@ -0,0 +1,110 @@
+#
+#
+#  Export SWC XTension  
+#
+#  @Biswanath Saha 
+#
+#    <CustomTools>
+#      <Menu>
+#       <Item name="SWC Exporter 1.0" icon="Python3" tooltip="Calls the Imaris To export SWC">
+#         <Command>Python3XT::XTExportSWC(%i)</Command>
+#       </Item>
+#      </Menu>
+#    </CustomTools>
+
+
+import ImarisLib
+import time
+import random
+import numpy as np
+import logging
+# GUI imports
+from tkinter import *
+from tkinter import messagebox
+from tkinter import simpledialog,filedialog
+logging.basicConfig(filename='G://swc_converter_log.txt', level=logging.DEBUG, 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+def XTExportSWC(aImarisId):
+	# Create an ImarisLib object
+	vImarisLib = ImarisLib.ImarisLib()
+
+	# Get an imaris object with id aImarisId
+	vImaris = vImarisLib.GetApplication(aImarisId)
+
+	if vImaris is None:
+		print('Could not connect to Imaris!')
+		time.sleep(10)
+		return
+	
+	vFactory = vImaris.GetFactory()
+	vFilaments = vFactory.ToFilaments(vImaris.GetSurpassSelection())
+
+	if vFilaments is None:
+		print('Pick a filament first')
+		time.sleep(10)
+		return
+	V = vImaris.GetDataSet()
+	pixel_scale = np.array([V.GetSizeX() / (V.GetExtendMaxX() - V.GetExtendMinX()),
+							V.GetSizeY() / (V.GetExtendMaxY() - V.GetExtendMinY()),
+							V.GetSizeZ() / (V.GetExtendMaxZ() - V.GetExtendMinZ())])
+	pixel_offset = np.array([V.GetExtendMinX(), V.GetExtendMinY(), V.GetExtendMinZ()])
+	# ad-hoc fix Z-flip when |maxZ| < |minZ|
+	if abs(V.GetExtendMinZ()) > abs(V.GetExtendMaxZ()):
+		pixel_offset = np.array([V.GetExtendMinX(), V.GetExtendMinY(), V.GetExtendMaxZ()])
+		pixel_scale[2] = -pixel_scale[2]
+
+	# get filename
+	root = Tk()
+	root.withdraw()
+	savename = filedialog.asksaveasfilename(defaultextension=".swc")
+	root.destroy()
+	if not savename: # asksaveasfilename return '' if dialog closed with "cancel".
+		print('No files selected')
+		time.sleep(10)
+		return
+	
+	#main conversion
+	swcs = np.zeros((0,7))
+	swc_list = []
+	vCount = vFilaments.GetNumberOfFilaments()
+	for i in range(vCount):
+		vFilamentsXYZ = vFilaments.GetPositionsXYZ(i)
+		vFilamentsRadius = vFilaments.GetRadii(i)
+		vFilamentsEdges = vFilaments.GetEdges(i)
+		vFilamentsTypes = vFilaments.GetTypes(i)
+
+		N = len(vFilamentsXYZ)
+		G = np.zeros((N,N),dtype=bool)
+		visited = np.zeros(N,dtype=bool)
+		for p1,p2 in vFilamentsEdges:
+			G[p1,p2] = True
+			G[p2,p1] = True
+			
+		head = 0
+		swc = np.zeros((N,7))
+		visited[0] = True
+
+
+		queue = [0]
+		prevs = [-1]
+		while queue:
+			cur = queue.pop()
+			prev = prevs.pop()
+
+			swc[head] = [head+1, vFilamentsTypes[cur],0,0,0,vFilamentsRadius[cur],prev]
+			pos = vFilamentsXYZ[cur]-pixel_offset
+			swc[head,2:5] = pos*pixel_scale
+			for idx in np.where(G[cur])[0]:
+				if not visited[idx]:
+					visited[idx] = True
+					queue.append(idx)
+					prevs.append(head+1)
+			head += 1
+		filename_filament = f"{savename}_filament_{i}.swc"
+		print('Exported '+str(i+1)+'/'+str(vCount)+' filaments',end='\r')
+		swcs = np.vstack((swcs,swc))
+		np.savetxt(filename_filament,swc,fmt='%d %d %f %f %f %f %d')
+	np.savetxt(savename,swcs,fmt='%d %d %f %f %f %f %d')
+	print('All filaments exported!')
+	time.sleep(5)
+		
---
+++ ImportSWC_new.py
@@ -0,0 +1,87 @@
+#
+#
+#  Import SWC XTension  
+#
+#  @Biswanath Saha
+#
+#    <CustomTools>
+#      <Menu>
+#       <Item name="SWC Importer 1.0" icon="Python3" tooltip="Calls the Imaris to import SWC">
+#         <Command>Python3XT::XTImportSWC(%i)</Command>
+#       </Item>
+#      </Menu>
+#    </CustomTools>
+
+
+import ImarisLib
+import time
+import random
+import numpy as np
+import logging
+# GUI imports
+from tkinter import *
+from tkinter import messagebox
+from tkinter import simpledialog,filedialog
+import os 
+logging.basicConfig(filename='G://swc_importer_log.txt', level=logging.DEBUG, 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+def XTImportSWC(aImarisId):
+	# Create an ImarisLib object
+	vImarisLib = ImarisLib.ImarisLib()
+
+	# Get an imaris object with id aImarisId
+	vImaris = vImarisLib.GetApplication(aImarisId)
+
+	if vImaris is None:
+		print('Could not connect to Imaris!')
+		time.sleep(10)
+		return
+	
+	vFactory = vImaris.GetFactory()
+	vFilaments = vFactory.ToFilaments(vImaris.GetSurpassSelection())
+	root = Tk()
+	root.withdraw()
+	swc_dir = filedialog.askdirectory(title='Select SWC Directory')
+	root.destroy()
+	if not swc_dir: # asksaveasfilename return '' if dialog closed with "cancel".
+		print('No files selected')
+		time.sleep(10)
+		return
+	print(swc_dir)
+	V = vImaris.GetDataSet()
+	pixel_scale = np.array([V.GetSizeX() / (V.GetExtendMaxX() - V.GetExtendMinX()),
+							V.GetSizeY() / (V.GetExtendMaxY() - V.GetExtendMinY()),
+							V.GetSizeZ() / (V.GetExtendMaxZ() - V.GetExtendMinZ())])
+	pixel_offset = np.array([V.GetExtendMinX(), V.GetExtendMinY(), V.GetExtendMinZ()])
+	# ad-hoc fix Z-flip when |maxZ| < |minZ|
+	if abs(V.GetExtendMinZ()) > abs(V.GetExtendMaxZ()):
+		pixel_offset = np.array([V.GetExtendMinX(), V.GetExtendMinY(), V.GetExtendMaxZ()])
+		pixel_scale[2] = -pixel_scale[2]
+	
+	for swc_file in os.listdir(swc_dir):
+				if swc_file.endswith(".swc"):
+					full_path = os.path.join(swc_dir, swc_file)
+					print('Importing: ' + full_path)
+					swc = np.loadtxt(full_path)
+
+					try:
+						N = swc.shape[0]
+						vFilaments = vImaris.GetFactory().CreateFilaments()
+						vPositions = swc[:, 2:5].astype(np.float) / pixel_scale
+						vPositions = vPositions + pixel_offset
+						vRadii = swc[:, 5].astype(np.float)
+						vTypes = np.zeros((N))  # (0: Dendrite; 1: Spine)
+						vEdges = swc[:, [6, 0]]
+						idx = np.all(vEdges > 0, axis=1)
+						vEdges = vEdges[idx, :] - 1
+						vTimeIndex = 0
+						vFilaments.AddFilament(vPositions.tolist(), vRadii.tolist(), vTypes.tolist(), vEdges.tolist(), vTimeIndex)
+						vFilamentIndex = 0
+						vVertexIndex = 1
+						vFilaments.SetBeginningVertexIndex(vFilamentIndex, vVertexIndex)
+						vScene = vImaris.GetSurpassScene()
+						vScene.AddChild(vFilaments, -1)
+						logging.info(f'Imported {swc_file}')
+					except Exception as e:
+						logging.error(f'Failed to import {swc_file}')
+	print('All swc files imported')
